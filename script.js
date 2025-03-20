1b2577e5da9245c33db6af6b2bf475a892df297f
arkan12-maker <muhammad_mariadi_33rpl@student.smktelkom-mlg.sch.id>
2025-03-20 21:02:26 +0700

    ccc

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 function sidebar() {
-    document.getElementById("mySidebar");
+   var x = document.getElementById("mySidebar");
+   var z = document.getElementById("textsb");
+   var y = document.getElementById("cont");
+   var a = document.getElementById("imgs");
+
+   if(x.style.display == "none") {
+       x.style.display = "block";
+       z.style.color = "white";
+     
+       y.style.transition = "ease-in-out";
+       y.style.transitionDuration = "1000ms";
+    
+   }else {
+       x.style.display = "none";
+       z.style.color = "black";
+      
+   
+       
+   } 
+
+
+   if(x.style.display == "block") {
+    z.style.marginTop = "50px";
+    a.style.marginTop = "40px";
+
+   }else {
+    z.style.marginTop = "-50px";
+    a.style.marginTop = "-60px";
+   }
+
+ 
+    
 }
